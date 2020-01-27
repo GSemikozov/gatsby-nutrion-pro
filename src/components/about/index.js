cc63142215da15d2ddef3f0ae3fab97b758c6790
hermansemykozov <semikozovgerman@gmail.com>
2020-01-27 22:56:47 +0200

    CHANGE: add desktop version of about section

--- src/components/about/index.js
+++ src/components/about/index.js
@@ -3,14 +3,20 @@ import React from 'react';
 
 import { Container } from '../container';
 import styles from './about.module.css';
+import IconDNA from './icons/icon-ui-dna.svg';
+import IconMeal from './icons/icon-ui-meal.svg';
+import IconPremium from './icons/icon-ui-premium.svg';
+import IconResearcher from './icons/icon-ui-researcher.svg';
+import IconSalad from './icons/icon-ui-salad.svg';
+import IconTruck from './icons/icon-ui-shipping-truck.svg';
 
 export const About = () => (
   <section className={styles.about}>
-    <Container>
+    <Container className={styles.container}>
       <h3 className={cx(styles.title, "sectionTitle -light")}>
         Proč NutritionPro?
       </h3>
-      <ul className={styles.aboutList}>
+      <ul className={cx(styles.aboutList, "visible-mobile")}>
         <li>Jídelníček na míru tvému tělu a cílům</li>
         <li>Přesně stanovené makronutrienty</li>
         <li>Stálý dohled výživového poradce</li>
@@ -18,6 +24,54 @@ export const About = () => (
         <li>Menu z 300 jídel</li>
         <li>Dovoz po Praze zdarma</li>
       </ul>
+      <div className={cx(styles.aboutOptions, "visible-desktop")}>
+        <div className={styles.aboutOption}>
+          <img src={IconMeal} className={styles.aboutOptionImg} alt="icon" />
+          <h5 className={styles.aboutOptionTitle}>
+            Jídelníček na míru
+            <br /> tvému tělu a cílům
+          </h5>
+        </div>
+        <div className={styles.aboutOption}>
+          <img src={IconDNA} className={styles.aboutOptionImg} alt="icon" />
+          <h5 className={styles.aboutOptionTitle}>
+            Přesně stanovené
+            <br /> makronutrienty
+          </h5>
+        </div>
+        <div className={styles.aboutOption}>
+          <img src={IconSalad} className={styles.aboutOptionImg} alt="icon" />
+          <h5 className={styles.aboutOptionTitle}>
+            Menu z
+            <br /> 300 jídel
+          </h5>
+        </div>
+        <div className={styles.aboutOption}>
+          <img src={IconPremium} className={styles.aboutOptionImg} alt="icon" />
+          <h5 className={styles.aboutOptionTitle}>
+            Kvalitní
+            <br /> prémiové suroviny
+          </h5>
+        </div>
+        <div className={styles.aboutOption}>
+          <img
+            src={IconResearcher}
+            className={styles.aboutOptionImg}
+            alt="icon"
+          />
+          <h5 className={styles.aboutOptionTitle}>
+            Stálý dohled
+            <br /> výživového poradce
+          </h5>
+        </div>
+        <div className={styles.aboutOption}>
+          <img src={IconTruck} className={styles.aboutOptionImg} alt="icon" />
+          <h5 className={styles.aboutOptionTitle}>
+            Dovoz po
+            <br /> Praze zdarma
+          </h5>
+        </div>
+      </div>
     </Container>
   </section>
 )
