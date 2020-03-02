b5225a5c1dcfc3bbb70fafab6c5a415e0f8b85d9
hermansemykozov <semikozovgerman@gmail.com>
2020-03-02 14:35:41 +0200

    site name

--- src/components/forms/main-form.js
+++ src/components/forms/main-form.js
@@ -137,7 +137,7 @@ export const MainFormLayout = ({
           </FastField> */}
           <FastField
             component="input"
-            type="tel"
+            type="text"
             name="phone"
             className={styles.input}
           />
@@ -207,9 +207,9 @@ export const MainForm = withFormik({
   }),
   validationSchema: () =>
     Yup.object().shape({
-      // phone: Yup.string()
-      //   .min(9)
-      //   .phone(),
+      phone: Yup.string()
+        .min(9)
+        .phone(),
       promo: Yup.string(),
       plan: Yup.string().required("Vyberte si program"),
       days: Yup.string().required("Vyberte si počet dní"),
--- src/components/seo.js
+++ src/components/seo.js
@@ -32,7 +32,7 @@ function SEO({ description, lang, meta, title }) {
         lang,
       }}
       title={title}
-      titleTemplate={`%s | ${site.siteMetadata.title}`}
+      titleTemplate={`NutritionPro - ${site.siteMetadata.title}`}
       meta={[
         {
           name: `description`,
