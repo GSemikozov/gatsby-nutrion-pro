bcd9ea6391ac81f68fefebc9ab3937a98731c6a3
hermansemykozov <semikozovgerman@gmail.com>
2020-03-02 14:13:58 +0200

    change type of phone number field

--- src/components/forms/main-form.js
+++ src/components/forms/main-form.js
@@ -207,9 +207,9 @@ export const MainForm = withFormik({
   }),
   validationSchema: () =>
     Yup.object().shape({
-      phone: Yup.string()
-        .min(9)
-        .phone(),
+      // phone: Yup.string()
+      //   .min(9)
+      //   .phone(),
       promo: Yup.string(),
       plan: Yup.string().required("Vyberte si program"),
       days: Yup.string().required("Vyberte si počet dní"),
