4bc7bf5c3bf7106cb0b826e7e400f668297d0382
hermansemykozov <semikozovgerman@gmail.com>
2020-03-18 12:25:07 +0200

    move back styles in template folder, rm unnecessary stuff

--- src/components/forms/index.js
+++
@@ -1,4 +0,0 @@
-import React from 'react';
-
-import { MainForm } from './main-form';
-
--- src/components/forms/input/index.js
+++
@@ -1,5 +0,0 @@
-import cx from 'classnames';
-import React from 'react';
-
-import styles from './input.module.css';
-
