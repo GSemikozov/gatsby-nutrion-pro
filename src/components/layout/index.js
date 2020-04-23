1cf5f84fe306a7b8e1fc3008b50edd9a826d5855
hermansemykozov <semikozovgerman@gmail.com>
2020-04-23 12:21:56 +0300

    CHANGE: move tag manager script into body

--- src/components/layout/index.js
+++ src/components/layout/index.js
@@ -32,16 +32,16 @@ export const Layout = ({ children }) => (
             jv-id="TLJ5HRqinQ"
             async
           />
-          <script type="text/javascript">
-            {`(function(w, d, s, h, id) {
+        </Helmet>
+        <script type="text/javascript">
+          {`(function(w, d, s, h, id) {
               w.roistatProjectId = id;
               w.roistatHost = h;
               var p = d.location.protocol == "https:" ? "https://" : "http://";
               var u = /^.*roistat_visit=[^;]+(.*)?$/.test(d.cookie) ? "/dist/module.js" : "/api/site/1.0/"+id+"/init";
               var js = d.createElement(s); js.charset="UTF-8"; js.async = 1; js.src = p+h+u; var js2 = d.getElementsByTagName(s)[0]; js2.parentNode.insertBefore(js, js2);
           })(window, document, 'script', 'cloud.roistat.com', '5ab3e816589c8e79e08e948e2d6e4313');`}
-          </script>
-        </Helmet>
+        </script>
         <div className="wrapper" id="root">
           <Header menuLinks={data.site.siteMetadata.menuLinks} />
           <>
