67249d35860ea3943d9e4f8296c61c3803b2e779
hermansemykozov <semikozovgerman@gmail.com>
2020-04-29 23:07:53 +0300

    CHANGE: simplify mobile foor carousel html

--- src/components/food/index.js
+++ src/components/food/index.js
@@ -195,32 +195,68 @@ const FoodCarouselMobile = () => {
     <Slider {...settings}>
       <div className={styles.carouselItem} key="1">
         <FoodCard img={img1} title="Kureci nudlicky s fazolovymi lusky">
-          <FoodCardList>
-            <FoodCardListItem text="268" title="kcal" />
-            <FoodCardListItem text="17g" title="sacharidy" />
-            <FoodCardListItem text="33g" title="bílkoviny" />
-            <FoodCardListItem text="4g" title="tuky" />
-          </FoodCardList>
+          <ul className={styles.foodCardList}>
+            <li>
+              <span className={styles.el}>268</span>
+              <span className={styles.title}>kcal</span>
+            </li>
+            <li>
+              <span className={styles.el}>17g</span>
+              <span className={styles.title}>sacharidy</span>
+            </li>
+            <li>
+              <span className={styles.el}>33g</span>
+              <span className={styles.title}>bílkoviny</span>
+            </li>
+            <li>
+              <span className={styles.el}>4g</span>
+              <span className={styles.title}>tuky</span>
+            </li>
+          </ul>
         </FoodCard>
       </div>
       <div className={styles.carouselItem} key="2">
         <FoodCard img={img2} title="Wrap s lososem">
-          <FoodCardList>
-            <FoodCardListItem text="333" title="kcal" />
-            <FoodCardListItem text="33g" title="sacharidy" />
-            <FoodCardListItem text="21g" title="bílkoviny" />
-            <FoodCardListItem text="13g" title="tuky" />
-          </FoodCardList>
+          <ul className={styles.foodCardList}>
+            <li>
+              <span className={styles.el}>333</span>
+              <span className={styles.title}>kcal</span>
+            </li>
+            <li>
+              <span className={styles.el}>33g</span>
+              <span className={styles.title}>sacharidy</span>
+            </li>
+            <li>
+              <span className={styles.el}>21g</span>
+              <span className={styles.title}>bílkoviny</span>
+            </li>
+            <li>
+              <span className={styles.el}>13g</span>
+              <span className={styles.title}>tuky</span>
+            </li>
+          </ul>
         </FoodCard>
       </div>
       <div className={styles.carouselItem} key="3">
         <FoodCard img={img3} title="Meal">
-          <FoodCardList>
-            <FoodCardListItem text="514" title="kcal" />
-            <FoodCardListItem text="43g" title="sacharidy" />
-            <FoodCardListItem text="32g" title="bílkoviny" />
-            <FoodCardListItem text="22g" title="tuky" />
-          </FoodCardList>
+          <ul className={styles.foodCardList}>
+            <li>
+              <span className={styles.el}>333</span>
+              <span className={styles.title}>kcal</span>
+            </li>
+            <li>
+              <span className={styles.el}>33g</span>
+              <span className={styles.title}>sacharidy</span>
+            </li>
+            <li>
+              <span className={styles.el}>21g</span>
+              <span className={styles.title}>bílkoviny</span>
+            </li>
+            <li>
+              <span className={styles.el}>13g</span>
+              <span className={styles.title}>tuky</span>
+            </li>
+          </ul>
         </FoodCard>
       </div>
       <div className={styles.carouselItem} key="4">
@@ -229,22 +265,46 @@ const FoodCarouselMobile = () => {
           title="Salat s roastbeefem
           a medovou zalivkou"
         >
-          <FoodCardList>
-            <FoodCardListItem text="186" title="kcal" />
-            <FoodCardListItem text="13g" title="sacharidy" />
-            <FoodCardListItem text="9g" title="bílkoviny" />
-            <FoodCardListItem text="11g" title="tuky" />
-          </FoodCardList>
+          <ul className={styles.foodCardList}>
+            <li>
+              <span className={styles.el}>333</span>
+              <span className={styles.title}>kcal</span>
+            </li>
+            <li>
+              <span className={styles.el}>33g</span>
+              <span className={styles.title}>sacharidy</span>
+            </li>
+            <li>
+              <span className={styles.el}>21g</span>
+              <span className={styles.title}>bílkoviny</span>
+            </li>
+            <li>
+              <span className={styles.el}>13g</span>
+              <span className={styles.title}>tuky</span>
+            </li>
+          </ul>
         </FoodCard>
       </div>
       <div className={styles.carouselItem} key="5">
         <FoodCard img={img5} title="Sendvic s krutim a kimchi omackou">
-          <FoodCardList>
-            <FoodCardListItem text="157" title="kcal" />
-            <FoodCardListItem text="16g" title="sacharidy" />
-            <FoodCardListItem text="12g" title="bílkoviny" />
-            <FoodCardListItem text="5g" title="tuky" />
-          </FoodCardList>
+          <ul className={styles.foodCardList}>
+            <li>
+              <span className={styles.el}>333</span>
+              <span className={styles.title}>kcal</span>
+            </li>
+            <li>
+              <span className={styles.el}>33g</span>
+              <span className={styles.title}>sacharidy</span>
+            </li>
+            <li>
+              <span className={styles.el}>21g</span>
+              <span className={styles.title}>bílkoviny</span>
+            </li>
+            <li>
+              <span className={styles.el}>13g</span>
+              <span className={styles.title}>tuky</span>
+            </li>
+          </ul>
         </FoodCard>
       </div>
       <div className={styles.carouselItem} key="6">
@@ -253,72 +313,156 @@ const FoodCarouselMobile = () => {
           title="Sklenene nudle 
           s hovezim a zeleninou "
         >
-          <FoodCardList>
-            <FoodCardListItem text="244" title="kcal" />
-            <FoodCardListItem text="23g" title="sacharidy" />
-            <FoodCardListItem text="25g" title="bílkoviny" />
-            <FoodCardListItem text="6g" title="tuky" />
-          </FoodCardList>
+          <ul className={styles.foodCardList}>
+            <li>
+              <span className={styles.el}>333</span>
+              <span className={styles.title}>kcal</span>
+            </li>
+            <li>
+              <span className={styles.el}>33g</span>
+              <span className={styles.title}>sacharidy</span>
+            </li>
+            <li>
+              <span className={styles.el}>21g</span>
+              <span className={styles.title}>bílkoviny</span>
+            </li>
+            <li>
+              <span className={styles.el}>13g</span>
+              <span className={styles.title}>tuky</span>
+            </li>
+          </ul>
         </FoodCard>
       </div>
       <div className={styles.carouselItem} key="7">
         <FoodCard img={img7} title="Nakyp bramborovy s masem">
-          <FoodCardList>
-            <FoodCardListItem text="468" title="kcal" />
-            <FoodCardListItem text="59g" title="sacharidy" />
-            <FoodCardListItem text="31g" title="bílkoviny" />
-            <FoodCardListItem text="12g" title="tuky" />
-          </FoodCardList>
+          <ul className={styles.foodCardList}>
+            <li>
+              <span className={styles.el}>333</span>
+              <span className={styles.title}>kcal</span>
+            </li>
+            <li>
+              <span className={styles.el}>33g</span>
+              <span className={styles.title}>sacharidy</span>
+            </li>
+            <li>
+              <span className={styles.el}>21g</span>
+              <span className={styles.title}>bílkoviny</span>
+            </li>
+            <li>
+              <span className={styles.el}>13g</span>
+              <span className={styles.title}>tuky</span>
+            </li>
+          </ul>
         </FoodCard>
       </div>
       <div className={styles.carouselItem} key="8">
         <FoodCard img={img8} title="Okoun s Kari omackou se zelim">
-          <FoodCardList>
-            <FoodCardListItem text="305" title="kcal" />
-            <FoodCardListItem text="18g" title="sacharidy" />
-            <FoodCardListItem text="28g" title="bílkoviny" />
-            <FoodCardListItem text="42g" title="tuky" />
-          </FoodCardList>
+          <ul className={styles.foodCardList}>
+            <li>
+              <span className={styles.el}>333</span>
+              <span className={styles.title}>kcal</span>
+            </li>
+            <li>
+              <span className={styles.el}>33g</span>
+              <span className={styles.title}>sacharidy</span>
+            </li>
+            <li>
+              <span className={styles.el}>21g</span>
+              <span className={styles.title}>bílkoviny</span>
+            </li>
+            <li>
+              <span className={styles.el}>13g</span>
+              <span className={styles.title}>tuky</span>
+            </li>
+          </ul>
         </FoodCard>
       </div>
       <div className={styles.carouselItem} key="9">
         <FoodCard img={img9} title="Pohankove karbanatky s cockou">
-          <FoodCardList>
-            <FoodCardListItem text="298" title="kcal" />
-            <FoodCardListItem text="20g" title="sacharidy" />
-            <FoodCardListItem text="20g" title="bílkoviny" />
-            <FoodCardListItem text="12g" title="tuky" />
-          </FoodCardList>
+          <ul className={styles.foodCardList}>
+            <li>
+              <span className={styles.el}>333</span>
+              <span className={styles.title}>kcal</span>
+            </li>
+            <li>
+              <span className={styles.el}>33g</span>
+              <span className={styles.title}>sacharidy</span>
+            </li>
+            <li>
+              <span className={styles.el}>21g</span>
+              <span className={styles.title}>bílkoviny</span>
+            </li>
+            <li>
+              <span className={styles.el}>13g</span>
+              <span className={styles.title}>tuky</span>
+            </li>
+          </ul>
         </FoodCard>
       </div>
       <div className={styles.carouselItem} key="10">
         <FoodCard img={img10} title="Bonbon s orechem">
-          <FoodCardList>
-            <FoodCardListItem text="509" title="kcal" />
-            <FoodCardListItem text="25g" title="sacharidy" />
-            <FoodCardListItem text="20g" title="bílkoviny" />
-            <FoodCardListItem text="48g" title="tuky" />
-          </FoodCardList>
+          <ul className={styles.foodCardList}>
+            <li>
+              <span className={styles.el}>333</span>
+              <span className={styles.title}>kcal</span>
+            </li>
+            <li>
+              <span className={styles.el}>33g</span>
+              <span className={styles.title}>sacharidy</span>
+            </li>
+            <li>
+              <span className={styles.el}>21g</span>
+              <span className={styles.title}>bílkoviny</span>
+            </li>
+            <li>
+              <span className={styles.el}>13g</span>
+              <span className={styles.title}>tuky</span>
+            </li>
+          </ul>
         </FoodCard>
       </div>
       <div className={styles.carouselItem} key="11">
         <FoodCard img={img11} title="Jablecne-mrkvove vafle">
-          <FoodCardList>
-            <FoodCardListItem text="217" title="kcal" />
-            <FoodCardListItem text="26g" title="sacharidy" />
-            <FoodCardListItem text="6g" title="bílkoviny" />
-            <FoodCardListItem text="11g" title="tuky" />
-          </FoodCardList>
+          <ul className={styles.foodCardList}>
+            <li>
+              <span className={styles.el}>333</span>
+              <span className={styles.title}>kcal</span>
+            </li>
+            <li>
+              <span className={styles.el}>33g</span>
+              <span className={styles.title}>sacharidy</span>
+            </li>
+            <li>
+              <span className={styles.el}>21g</span>
+              <span className={styles.title}>bílkoviny</span>
+            </li>
+            <li>
+              <span className={styles.el}>13g</span>
+              <span className={styles.title}>tuky</span>
+            </li>
+          </ul>
         </FoodCard>
       </div>
       <div className={styles.carouselItem} key="12">
         <FoodCard img={img12} title="Tykvovy nakyp">
-          <FoodCardList>
-            <FoodCardListItem text="256" title="kcal" />
-            <FoodCardListItem text="12g" title="sacharidy" />
-            <FoodCardListItem text="23g" title="bílkoviny" />
-            <FoodCardListItem text="13g" title="tuky" />
-          </FoodCardList>
+          <ul className={styles.foodCardList}>
+            <li>
+              <span className={styles.el}>333</span>
+              <span className={styles.title}>kcal</span>
+            </li>
+            <li>
+              <span className={styles.el}>33g</span>
+              <span className={styles.title}>sacharidy</span>
+            </li>
+            <li>
+              <span className={styles.el}>21g</span>
+              <span className={styles.title}>bílkoviny</span>
+            </li>
+            <li>
+              <span className={styles.el}>13g</span>
+              <span className={styles.title}>tuky</span>
+            </li>
+          </ul>
         </FoodCard>
       </div>
     </Slider>
