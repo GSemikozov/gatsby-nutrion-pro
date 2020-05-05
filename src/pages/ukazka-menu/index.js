a95ed5aaff1d440ae9fd52e19de9bac5ceff16cb
hermansemykozov <semikozovgerman@gmail.com>
2020-05-05 21:27:22 +0300

    CHANGE: fix table texts in ukazka-menu page

--- src/pages/ukazka-menu/index.js
+++ src/pages/ukazka-menu/index.js
@@ -15,25 +15,25 @@ const MenuPage = () => {
         <div className={styles.menuGrid}>
           <section>
             <h3 className={styles.menuTitle}>
-              <span>Pondeli</span>
+              <span>Pondělí</span>
             </h3>
             <table className={styles.menuTable}>
               <tr>
-                <td>Snidane</td>
+                <td>Snídaně</td>
                 <td>
                   <img src={dishIcon} alt="dish-icon" />
                   Omeletové proužky s ančovičkami
                 </td>
               </tr>
               <tr>
-                <td>Svacina</td>
+                <td>Svačina</td>
                 <td>
                   <img src={dishIcon} alt="dish-icon" />
                   Rolky s limetkovým tvarohem a ovocem
                 </td>
               </tr>
               <tr>
-                <td>Obed</td>
+                <td>Oběd</td>
                 <td>
                   <div>
                     <img src={dishIcon} alt="dish-icon" />
@@ -46,14 +46,14 @@ const MenuPage = () => {
                 </td>
               </tr>
               <tr>
-                <td>Svacina</td>
+                <td>Svačina</td>
                 <td>
                   <img src={dishIcon} alt="dish-icon" />
                   Ragú z mořských plodů
                 </td>
               </tr>
               <tr>
-                <td>Vecere</td>
+                <td>Večeře</td>
                 <td>
                   <img src={dishIcon} alt="dish-icon" />
                   Frikasé z krůty a pohanková se zeleninou
@@ -63,25 +63,25 @@ const MenuPage = () => {
           </section>
           <section>
             <h3 className={styles.menuTitle}>
-              <span>Utery</span>
+              <span>Úterý</span>
             </h3>
             <table className={styles.menuTable}>
               <tr>
-                <td>Snidane</td>
+                <td>Snídaně</td>
                 <td>
                   <img src={dishIcon} alt="dish-icon" />
                   Volské oko se směsí zeleniny
                 </td>
               </tr>
               <tr>
-                <td>Svacina</td>
+                <td>Svačina</td>
                 <td>
                   <img src={dishIcon} alt="dish-icon" />
                   Ovoce v agarovém želé
                 </td>
               </tr>
               <tr>
-                <td>Obed</td>
+                <td>Oběd</td>
                 <td>
                   <div>
                     <img src={dishIcon} alt="dish-icon" />
@@ -94,14 +94,14 @@ const MenuPage = () => {
                 </td>
               </tr>
               <tr>
-                <td>Svacina</td>
+                <td>Svačina</td>
                 <td>
                   <img src={dishIcon} alt="dish-icon" />
                   Kokosové palačinky s krevetami a sýrem stracciatella
                 </td>
               </tr>
               <tr>
-                <td>Vecere</td>
+                <td>Večeře</td>
                 <td>
                   <img src={dishIcon} alt="dish-icon" />
                   Cuketové špagety s kuřecím masem a pestem
@@ -111,25 +111,25 @@ const MenuPage = () => {
           </section>
           <section>
             <h3 className={styles.menuTitle}>
-              <span>Streda</span>
+              <span>Středa</span>
             </h3>
             <table className={styles.menuTable}>
               <tr>
-                <td>Snidane</td>
+                <td>Snídaně</td>
                 <td>
                   <img src={dishIcon} alt="dish-icon" />
                   Palačinky z sýrem a rajčaty
                 </td>
               </tr>
               <tr>
-                <td>Svacina</td>
+                <td>Svačina</td>
                 <td>
                   <img src={dishIcon} alt="dish-icon" />
                   Ananasy grilované s borůvkami
                 </td>
               </tr>
               <tr>
-                <td>Obed</td>
+                <td>Oběd</td>
                 <td>
                   <div>
                     <img src={dishIcon} alt="dish-icon" />
@@ -142,14 +142,14 @@ const MenuPage = () => {
                 </td>
               </tr>
               <tr>
-                <td>Svacina</td>
+                <td>Svačina</td>
                 <td>
                   <img src={dishIcon} alt="dish-icon" />
                   Salát Nicoise s tuňákem a fazolemi
                 </td>
               </tr>
               <tr>
-                <td>Vecere</td>
+                <td>Večeře</td>
                 <td>
                   <img src={dishIcon} alt="dish-icon" />
                   Losos gorbuša zapečený se zeleninou v páře
@@ -159,25 +159,25 @@ const MenuPage = () => {
           </section>
           <section>
             <h3 className={styles.menuTitle}>
-              <span>Ctvrtek</span>
+              <span>Čtvrtek</span>
             </h3>
             <table className={styles.menuTable}>
               <tr>
-                <td>Snidane</td>
+                <td>Snídaně</td>
                 <td>
                   <img src={dishIcon} alt="dish-icon" />
                   Omeleta s avokádem, rajčaty a sýrem
                 </td>
               </tr>
               <tr>
-                <td>Svacina</td>
+                <td>Svačina</td>
                 <td>
                   <img src={dishIcon} alt="dish-icon" />
                   Univerzální salát z pasty
                 </td>
               </tr>
               <tr>
-                <td>Obed</td>
+                <td>Oběd</td>
                 <td>
                   <div>
                     <img src={dishIcon} alt="dish-icon" />
@@ -190,14 +190,14 @@ const MenuPage = () => {
                 </td>
               </tr>
               <tr>
-                <td>Svacina</td>
+                <td>Svačina</td>
                 <td>
                   <img src={dishIcon} alt="dish-icon" />
                   Salát z mořských plodů
                 </td>
               </tr>
               <tr>
-                <td>Vecere</td>
+                <td>Večeře</td>
                 <td>
                   <img src={dishIcon} alt="dish-icon" />
                   Lenivý brizol s houbovou omáčkou
@@ -207,25 +207,25 @@ const MenuPage = () => {
           </section>
           <section>
             <h3 className={styles.menuTitle}>
-              <span>Patek</span>
+              <span>Pátek</span>
             </h3>
             <table className={styles.menuTable}>
               <tr>
-                <td>Snidane</td>
+                <td>Snídaně</td>
                 <td>
                   <img src={dishIcon} alt="dish-icon" />
                   Tvarohové lívanečky se smetanou
                 </td>
               </tr>
               <tr>
-                <td>Svacina</td>
+                <td>Svačina</td>
                 <td>
                   <img src={dishIcon} alt="dish-icon" />
                   Hruška s ořechy a čokoládou
                 </td>
               </tr>
               <tr>
-                <td>Obed</td>
+                <td>Oběd</td>
                 <td>
                   <div>
                     <img src={dishIcon} alt="dish-icon" />
@@ -238,14 +238,14 @@ const MenuPage = () => {
                 </td>
               </tr>
               <tr>
-                <td>Svacina</td>
+                <td>Svačina</td>
                 <td>
                   <img src={dishIcon} alt="dish-icon" />
                   Rolka s krabem
                 </td>
               </tr>
               <tr>
-                <td>Vecere</td>
+                <td>Večeře</td>
                 <td>
                   <img src={dishIcon} alt="dish-icon" />
                   Kuřecí karbanátek s tvarohem a bazalkou a zeleninou v marinádě
@@ -259,21 +259,21 @@ const MenuPage = () => {
             </h3>
             <table className={styles.menuTable}>
               <tr>
-                <td>Snidane</td>
+                <td>Snídaně</td>
                 <td>
                   <img src={dishIcon} alt="dish-icon" />
                   Palačinky špenátovo-zázvorové z amarantové mouky
                 </td>
               </tr>
               <tr>
-                <td>Svacina</td>
+                <td>Svačina</td>
                 <td>
                   <img src={dishIcon} alt="dish-icon" />
                   Tvarohový dezert s avokádem
                 </td>
               </tr>
               <tr>
-                <td>Obed</td>
+                <td>Oběd</td>
                 <td>
                   <div>
                     <img src={dishIcon} alt="dish-icon" />
@@ -286,14 +286,14 @@ const MenuPage = () => {
                 </td>
               </tr>
               <tr>
-                <td>Svacina</td>
+                <td>Svačina</td>
                 <td>
                   <img src={dishIcon} alt="dish-icon" />
                   Toast s domácí šunkou
                 </td>
               </tr>
               <tr>
-                <td>Vecere</td>
+                <td>Večeře</td>
                 <td>
                   <img src={dishIcon} alt="dish-icon" />
                   Steak z lososa s pečenou zeleninou
