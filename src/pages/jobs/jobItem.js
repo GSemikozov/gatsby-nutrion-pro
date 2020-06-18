93c7985bac09ac76f4e6722bd1e240a8bc7fce61
hermansemykozov <semikozovgerman@gmail.com>
2020-06-18 18:45:35 +0300

    CHANGE: update job position

--- src/pages/jobs/jobItem.js
+++ src/pages/jobs/jobItem.js
@@ -28,9 +28,9 @@ const renderJobs = () => [
   // />,
   <Job
     key="2"
-    title="Obchodní zástupce"
+    title="Sales manager"
     description="Do našeho mladého týmu hledáme šikovného/ou kolegu/yni na pozici Obchodní zástupce."
-    date="12.12.2019"
+    date="18.06.2020"
     color="#EAEFF7"
     link="/jobs/sales"
   />,
