21995bb221af92c77389fa453c8ba9364ed43575
hermansemykozov <semikozovgerman@gmail.com>
2021-03-04 12:04:37 +0200

    create empty components under v2

--- src/components/layout/index.js
+++ src/components/layout/index.js
@@ -6,14 +6,9 @@ import PropTypes from 'prop-types';
 import React from 'react';
 import Helmet from 'react-helmet';
 
-import { withTrans } from '../../i18n/withTrans';
-import { LangProvider } from '../../utils/lang';
 import { Footer } from '../footer';
 import { Header } from '../header';
-import { PreHeader } from '../preheader';
 
-// import { I18nextProvider } from 'react-i18next';
-// import i18next from './i18next';
 const Layout = ({ children, t, i18n, location }) => (
   <StaticQuery
     query={graphql`
@@ -40,7 +35,6 @@ const Layout = ({ children, t, i18n, location }) => (
           />
         </Helmet>
         <div className="wrapper" id="root">
-          {/* <PreHeader /> */}
           <Header menuLinks={data.site.siteMetadata.menuLinks} />
           <>
             <main>{children}</main>
@@ -57,5 +51,3 @@ Layout.propTypes = {
 }
 
 export default Layout
-
-// export default withTrans(Layout)
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,38 +1,12 @@
 import React from 'react';
 
-import { About } from '../components/about';
-import { Calculator2 } from '../components/calculator2';
-import { FAQ } from '../components/faq';
-import { FoodCarouselSection } from '../components/food';
-import { Hero } from '../components/hero2';
-import { HomeofficePackages } from '../components/homeoffice-packages';
-import { HowItWork } from '../components/howitwork';
-import { Order } from '../components/order-form';
-import { PreFooter } from '../components/prefooter';
-import { Program } from '../components/program';
-import { Reviews } from '../components/reviews';
 import SEO from '../components/seo';
-import { TryFood } from '../components/tryfood';
 
-// import Layout from '../components/layout';
 const IndexPage = () => (
   <>
-    <SEO title="Home" />
-    <Hero />
-    <Program id="programs" />
-    {/* <HomeofficePackages /> */}
-    {/* <Calculator2 id="calculator" /> */}
-    <Order id="calculator" />
-    <FoodCarouselSection />
-    <About />
-    {/* <TryFood id="try-food" /> */}
-    <Reviews id="reviews" />
-    <HowItWork />
-    {/* <FAQ id="faq" /> */}
-    <PreFooter />
+    <SEO title="HomeNew" />
+    {/* components */}
   </>
 )
-
-// ReactDOM.render(<App />, document.getElementById("root"))
 
 export default IndexPage
