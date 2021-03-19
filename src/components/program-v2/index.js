c906f3a07a99289a108cc9c62748c627631bf399
Oleg <bukreevoleg3001@gmail.com>
2021-03-19 02:22:39 +0200

    add img card, css, media

--- src/components/program-v2/index.js
+++ src/components/program-v2/index.js
@@ -1,8 +1,9 @@
 import React from 'react';
+import cx from 'classnames';
 
 import { Container } from '../container';
-
 import styles from './program.module.css';
+
 
 export const Program = () => {
   return (
@@ -13,28 +14,28 @@ export const Program = () => {
         </h3>
         <div className={styles.sectionList}>
           <div className={styles.sectionListItem}>
-            <div className={styles.sectionListImg}></div>
+            <div className={cx(styles.sectionListImg, styles.sectionListImg1)}></div>            
             <div className={styles.title}>
             <span>Hubnoucí program</span>
             </div>
           </div>
 
           <div className={styles.sectionListItem}>
-            <div className={styles.sectionListImg}></div>
+          <div className={cx(styles.sectionListImg, styles.sectionListImg2)}></div>
             <div className={styles.title}>
             <span>Nabírací program</span>
             </div>
           </div>
 
           <div className={styles.sectionListItem}>
-            <div className={styles.sectionListImg}></div>
+          <div className={cx(styles.sectionListImg, styles.sectionListImg3)}></div>
             <div className={styles.title}>
             <span>Udržovací program</span>
             </div>
           </div>
 
           <div className={styles.sectionListItem}>
-            <div className={styles.sectionListImg}></div>
+          <div className={cx(styles.sectionListImg, styles.sectionListImg4)}></div>
             <div className={styles.title}>
             <span>Office pack</span>
             </div>
