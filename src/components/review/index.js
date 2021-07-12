e8e2f06c5fb9c39a0beecf92391a1e5985c892ef
hermansemykozov <semikozovgerman@gmail.com>
2021-07-12 12:27:36 +0300

    hide date in review card

--- src/components/review/index.js
+++ src/components/review/index.js
@@ -3,214 +3,257 @@ import React from 'react';
 import { ReviewCard } from './card';
 import styles from './review.module.css';
 
+const reviews = [
+  {
+    id: "",
+    name: "",
+    date: "",
+    text: "",
+  },
+]
+
 export const Review = () => {
   return (
-    <div className={styles.review}>   
-    <ReviewCard 
-    name="Matěj Schultz"
-    date="20.04.2021"
-    text="Nejlepší poměr cena/kvalita, neskutečně jste mi pomohli, moc děkuji!!" /> 
+    <div className={styles.review}>
+      <ReviewCard
+        name="Matěj Schultz"
+        date="20.04.2021"
+        text="Nejlepší poměr cena/kvalita, neskutečně jste mi pomohli, moc děkuji!!"
+      />
 
-    <ReviewCard 
-    name="Klára Onderková"
-    date="20.04.2021"
-    text="Vždy dovoz včas a supr servis, díky za ušetřený čas nutrition pro!!" /> 
+      <ReviewCard
+        name="Klára Onderková"
+        date="20.04.2021"
+        text="Vždy dovoz včas a supr servis, díky za ušetřený čas nutrition pro!!"
+      />
 
-    <ReviewCard 
-    name="Ludmila Koneva"
-    date="20.04.2021"
-    text="Velice oceňuji ochotu servisu a skvělé jídlo, zkoušela jsem Fitkitchen a Zdrave stravovani a musím říct, že nebyla jsem s nimi ani blízko spokojena jako s vámi, děkuji děkuji nenapíšem správně naše jméno?"/> 
+      <ReviewCard
+        name="Ludmila Koneva"
+        date="20.04.2021"
+        text="Velice oceňuji ochotu servisu a skvělé jídlo, zkoušela jsem Fitkitchen a Zdrave stravovani a musím říct, že nebyla jsem s nimi ani blízko spokojena jako s vámi, děkuji děkuji nenapíšem správně naše jméno?"
+      />
 
-    <ReviewCard 
-    name="Aneta pecková"
-    date="20.04.2021"
-    text="možná první podobný servis, který mi během prvního týdnu nezklamal, dokonce naopak, paráda!!"/> 
+      <ReviewCard
+        name="Aneta pecková"
+        date="20.04.2021"
+        text="možná první podobný servis, který mi během prvního týdnu nezklamal, dokonce naopak, paráda!!"
+      />
 
-    <ReviewCard 
-    name="Gregory Pelec"
-    date="20.04.2021"
-    text="Its been a while since I started my plan with you and surpisingly every meal was very delicious and the kilos went down finally!! Highly recommend The Home office pack to those who spend everyday in the office as I do "/> 
+      <ReviewCard
+        name="Gregory Pelec"
+        date="20.04.2021"
+        text="Its been a while since I started my plan with you and surpisingly every meal was very delicious and the kilos went down finally!! Highly recommend The Home office pack to those who spend everyday in the office as I do "
+      />
 
-    <ReviewCard 
-    name="Jitka Beranová"
-    date="20.04.2021"
-    text="Blíží se ke konci první měsíc co jsem s Vámi a už se citím tísickrát lépe, minus 3 kila a konečně mám čas a energii, jste naprosto Boží"/> 
- 
-    <ReviewCard 
-    name="Petr Hrdina"
-    date="20.04.2021"
-    text="Myslel jsem si, ze uz nikdy nezhubnu, ani nematu predstavu, jak jsem vam vdecny"/> 
-   
-    <ReviewCard 
-    name="Nicole Chandy"
-    date="20.04.2021"
-    text="Wanted to share my thoughts for a while already and its time to do so: the food is almost at a restaurant level, so is the servis. Love at first sight (or at the first taste if you know what I mean)"/> 
-   
-    <ReviewCard 
-    name="Monika Horáková"
-    date="20.04.2021"
-    text="musim vam poděkovat za mou postavu a ušetřený čas lidé moc doporučuji"/> 
-   
-    <ReviewCard 
-    name="Анна Алексеева"
-    date="20.04.2021"
-    text="Очень бы хотелось оценить высокое качество сервиса на том конце телефона, молодцы"/> 
-   
-    <ReviewCard 
-    name="Jindra Chmátal"
-    date="20.04.2021"
-    text="Výnikající ryba, nevím, jak to děláte, ale pro ní radší za váma než do restaurace"/> 
-   
-    <ReviewCard 
-    name="zděnek r"
-    date="20.04.2021"
-    text="nemohl jsem pribrat ani kilo a dostal jsem na vas doporuceni od kamarada ktery vas objednava, po trech mesicich jsem pribral 4 kila! fakt velky diky vasemu dietologovi"/> 
-   
-    <ReviewCard 
-    name="Kateřina Legnerová"
-    date="20.04.2021"
-    text="Zamilovala jsem se do vašich jídel"/> 
-   
-    <ReviewCard 
-    name="Terka Říčánová"
-    date="20.04.2021"
-    text="máte to výborné, dokonce se mi občas vás chce zeptat na recepty"/> 
-   
-    <ReviewCard 
-    name="Alžběta Doubková"
-    date="20.04.2021"
-    text="Poklona kuchařům"/> 
-   
-    <ReviewCard 
-    name="Jan Hamáček"
-    date="20.04.2021"
-    text="První dieta opravdu odpovidající popisu, je úplně skvělé jak všechno máte promyšlené do detailů"/> 
-   
-    <ReviewCard 
-    name="Maxim Abramov"
-    date="20.04.2021"
-    text="After almost six month of ordering you I`m still satisfied. I didn`t even expect such a diversed meal plan gyus"/> 
-   
-    <ReviewCard 
-    name="Eva Brouzkova"
-    date="20.04.2021"
-    text="Jste asi nejlepsi krabicova dieta kterou jsem kdy zkousela a mela jsem jich fakt hodne, salaty jsou naprosto perfektni"/> 
-   
-    <ReviewCard 
-    name="Ondřej Suchánek"
-    date="20.04.2021"
-    text="S přitelkyni jsme vás před chvíli objednali a musím říct, že jen tak dal, famozní jídla a kompetentní servis, todle jsem ani nečekal"/> 
-   
-    <ReviewCard 
-    name="Vít Mikuláš"
-    date="20.04.2021"
-    text="Takovou chuť jsem úpřímně za tu cenu nečekal, kéž bych o vás věděl dříve, byl bych na létuo přípravený"/> 
-   
-    <ReviewCard 
-    name="ollie fox"
-    date="20.04.2021"
-    text="super jídla a super telefonický servis, konečně někdo dokáže sestavit individuální program bez blbostí"/> 
-   
-    <ReviewCard 
-    name="Monika Sedlačková"
-    date="20.04.2021"
-    text="S manželem jsme nikdy nebyli spokojenější, děkujeme za vstříčnost"/> 
-   
-    <ReviewCard 
-    name="Jírka"
-    date="20.04.2021"
-    text="Mockrát mi pomohl ten váš hundoucí program, po dlouhé době, co se snažím zhubnout konečně něco fungujícího , děkuji"/> 
-   
-    <ReviewCard 
-    name="Jan Jan"
-    date="20.04.2021"
-    text="Moc se mi líbí že jídla jsou opravdu vyvážené a k tomu dobře chutnájí, nemám co podotknout jste skvělí moc děkuji"/> 
-   
-    <ReviewCard 
-    name="Hanka Moravcová"
-    date="20.04.2021"
-    text="Vaše jídla mi doporučil tréner za což jsem mu velice vděčná, pomohli jste mi naplnit cíle po dlouhé stagnaci, doporučuji všem!!!"/> 
-   
-    <ReviewCard 
-    name="Gena H"
-    date="20.04.2021"
-    text="chci vám osobně poděkovat za snídaně a svačiny konečně jsem se vzdál brambůrek a dortů"/> 
-   
-    <ReviewCard 
-    name="Pol Plotzky"
-    date="20.04.2021"
-    text="Undoubtedly the best diet in Prague meanwhile especially due to the quality of servis and management and of course the quality of the food, huge thanks"/> 
-   
-    <ReviewCard 
-    name="honza přibral"
-    date="20.04.2021"
-    text="teď už pokud jídlo s dovozem, tak žádný mekáč, jen vy"/> 
-   
-    <ReviewCard 
-    name="Simona Haganová"
-    date="20.04.2021"
-    text='Musím ocenit s jakou přesností doručujete jídla, možná první "krabičková dieta", která své sliby opravdu dodržuje, dále jen tak prosím'/> 
-   
-    <ReviewCard 
-    name="Jindra Býk"
-    date="20.04.2021"
-    text="Nadmíru jste moje očekávaní splnili co se týče chutě, pan po telefonu je náprosty profesional, fakt vám rád volám :)"/> 
-   
-    <ReviewCard 
-    name="Арина Клюева"
-    date="20.04.2021"
-    text="Супер вкусно, супер полезно, все посчитали сами, обо всем позаботились. Раудет, что хороший сервис еще хоть где-то остался) Про еду вообще молчу)"/> 
-   
-    <ReviewCard 
-    name="David David"
-    date="20.04.2021"
-    text="Moje uznání celé firmě a prosím držet tuto kvalitu"/> 
-   
-    <ReviewCard 
-    name="draho mírný"
-    date="20.04.2021"
-    text="začal jsem posilovat ještě před rokem a měl jsem velký problém ohledně nabírání svalové hmoty, nepomáhaly mě ani protejnaky ani tuny masa teď ale po pár měsících s váma +4 kila svalů nevím co v tom jídlu máte ale moc vám děkuji"/> 
-   
-    <ReviewCard 
-    name="Conor Lids"
-    date="20.04.2021"
-    text="The most suitable office pack for office workers (me), having your meals on the desk instead of unhealthy snacks is a habit aready, thank you for my energy and time saved NP"/> 
-   
-    <ReviewCard 
-    name="Elišká Novotná"
-    date="20.04.2021"
-    text="Zase bych Vám chtěla poděkovat za až natolik rozmanitý jídelníček a to samé za manžela"/> 
-   
-    <ReviewCard 
-    name="Petr Max"
-    date="20.04.2021"
-    text="Nepíšu normálně recenze ale fakt musím říct, že jsem nedostal ještě jídlo, které by mi nechutnalo, trefíte se vždy přesně"/> 
-   
-    <ReviewCard 
-    name="Šája Charlotta"
-    date="20.04.2021"
-    text="Nadšena jsem zákaznickým přístupem, úplně jiný, řekla bych západní level"/> 
-   
-    <ReviewCard 
-    name="Adam Kr"
-    date="20.04.2021"
-    text="možná jedna z nejlepších podobných firem, zkoušel jsem jich mnoho a po prvním měsici s vámi už vím, že zůstávám"/> 
-   
-    <ReviewCard 
-    name="Kristynka"
-    date="20.04.2021"
-    text="dnešní večeře naprosto bomba nelitovali jsme že jsme nešli pro jídlo ven"/> 
-   
-    <ReviewCard 
-    name="Tereza Petrova"
-    date="20.04.2021"
-    text="Bojím se vás vynachválit aby všechno zůstalo jak to je ale musím, chutnálo mi zatím absolutně každé jídlo, zejména dezerty jsou aaach" /> 
+      <ReviewCard
+        name="Jitka Beranová"
+        date="20.04.2021"
+        text="Blíží se ke konci první měsíc co jsem s Vámi a už se citím tísickrát lépe, minus 3 kila a konečně mám čas a energii, jste naprosto Boží"
+      />
 
+      <ReviewCard
+        name="Petr Hrdina"
+        date="20.04.2021"
+        text="Myslel jsem si, ze uz nikdy nezhubnu, ani nematu predstavu, jak jsem vam vdecny"
+      />
+
+      <ReviewCard
+        name="Nicole Chandy"
+        date="20.04.2021"
+        text="Wanted to share my thoughts for a while already and its time to do so: the food is almost at a restaurant level, so is the servis. Love at first sight (or at the first taste if you know what I mean)"
+      />
+
+      <ReviewCard
+        name="Monika Horáková"
+        date="20.04.2021"
+        text="musim vam poděkovat za mou postavu a ušetřený čas lidé moc doporučuji"
+      />
+
+      <ReviewCard
+        name="Анна Алексеева"
+        date="20.04.2021"
+        text="Очень бы хотелось оценить высокое качество сервиса на том конце телефона, молодцы"
+      />
+
+      <ReviewCard
+        name="Jindra Chmátal"
+        date="20.04.2021"
+        text="Výnikající ryba, nevím, jak to děláte, ale pro ní radší za váma než do restaurace"
+      />
+
+      <ReviewCard
+        name="zděnek r"
+        date="20.04.2021"
+        text="nemohl jsem pribrat ani kilo a dostal jsem na vas doporuceni od kamarada ktery vas objednava, po trech mesicich jsem pribral 4 kila! fakt velky diky vasemu dietologovi"
+      />
+
+      <ReviewCard
+        name="Kateřina Legnerová"
+        date="20.04.2021"
+        text="Zamilovala jsem se do vašich jídel"
+      />
+
+      <ReviewCard
+        name="Terka Říčánová"
+        date="20.04.2021"
+        text="máte to výborné, dokonce se mi občas vás chce zeptat na recepty"
+      />
+
+      <ReviewCard
+        name="Alžběta Doubková"
+        date="20.04.2021"
+        text="Poklona kuchařům"
+      />
+
+      <ReviewCard
+        name="Jan Hamáček"
+        date="20.04.2021"
+        text="První dieta opravdu odpovidající popisu, je úplně skvělé jak všechno máte promyšlené do detailů"
+      />
+
+      <ReviewCard
+        name="Maxim Abramov"
+        date="20.04.2021"
+        text="After almost six month of ordering you I`m still satisfied. I didn`t even expect such a diversed meal plan gyus"
+      />
+
+      <ReviewCard
+        name="Eva Brouzkova"
+        date="20.04.2021"
+        text="Jste asi nejlepsi krabicova dieta kterou jsem kdy zkousela a mela jsem jich fakt hodne, salaty jsou naprosto perfektni"
+      />
+
+      <ReviewCard
+        name="Ondřej Suchánek"
+        date="20.04.2021"
+        text="S přitelkyni jsme vás před chvíli objednali a musím říct, že jen tak dal, famozní jídla a kompetentní servis, todle jsem ani nečekal"
+      />
+
+      <ReviewCard
+        name="Vít Mikuláš"
+        date="20.04.2021"
+        text="Takovou chuť jsem úpřímně za tu cenu nečekal, kéž bych o vás věděl dříve, byl bych na létuo přípravený"
+      />
+
+      <ReviewCard
+        name="ollie fox"
+        date="20.04.2021"
+        text="super jídla a super telefonický servis, konečně někdo dokáže sestavit individuální program bez blbostí"
+      />
+
+      <ReviewCard
+        name="Monika Sedlačková"
+        date="20.04.2021"
+        text="S manželem jsme nikdy nebyli spokojenější, děkujeme za vstříčnost"
+      />
+
+      <ReviewCard
+        name="Jírka"
+        date="20.04.2021"
+        text="Mockrát mi pomohl ten váš hundoucí program, po dlouhé době, co se snažím zhubnout konečně něco fungujícího , děkuji"
+      />
+
+      <ReviewCard
+        name="Jan Jan"
+        date="20.04.2021"
+        text="Moc se mi líbí že jídla jsou opravdu vyvážené a k tomu dobře chutnájí, nemám co podotknout jste skvělí moc děkuji"
+      />
+
+      <ReviewCard
+        name="Hanka Moravcová"
+        date="20.04.2021"
+        text="Vaše jídla mi doporučil tréner za což jsem mu velice vděčná, pomohli jste mi naplnit cíle po dlouhé stagnaci, doporučuji všem!!!"
+      />
+
+      <ReviewCard
+        name="Gena H"
+        date="20.04.2021"
+        text="chci vám osobně poděkovat za snídaně a svačiny konečně jsem se vzdál brambůrek a dortů"
+      />
+
+      <ReviewCard
+        name="Pol Plotzky"
+        date="20.04.2021"
+        text="Undoubtedly the best diet in Prague meanwhile especially due to the quality of servis and management and of course the quality of the food, huge thanks"
+      />
+
+      <ReviewCard
+        name="honza přibral"
+        date="20.04.2021"
+        text="teď už pokud jídlo s dovozem, tak žádný mekáč, jen vy"
+      />
+
+      <ReviewCard
+        name="Simona Haganová"
+        date="20.04.2021"
+        text='Musím ocenit s jakou přesností doručujete jídla, možná první "krabičková dieta", která své sliby opravdu dodržuje, dále jen tak prosím'
+      />
+
+      <ReviewCard
+        name="Jindra Býk"
+        date="20.04.2021"
+        text="Nadmíru jste moje očekávaní splnili co se týče chutě, pan po telefonu je náprosty profesional, fakt vám rád volám :)"
+      />
+
+      <ReviewCard
+        name="Арина Клюева"
+        date="20.04.2021"
+        text="Супер вкусно, супер полезно, все посчитали сами, обо всем позаботились. Раудет, что хороший сервис еще хоть где-то остался) Про еду вообще молчу)"
+      />
+
+      <ReviewCard
+        name="David David"
+        date="20.04.2021"
+        text="Moje uznání celé firmě a prosím držet tuto kvalitu"
+      />
+
+      <ReviewCard
+        name="draho mírný"
+        date="20.04.2021"
+        text="začal jsem posilovat ještě před rokem a měl jsem velký problém ohledně nabírání svalové hmoty, nepomáhaly mě ani protejnaky ani tuny masa teď ale po pár měsících s váma +4 kila svalů nevím co v tom jídlu máte ale moc vám děkuji"
+      />
+
+      <ReviewCard
+        name="Conor Lids"
+        date="20.04.2021"
+        text="The most suitable office pack for office workers (me), having your meals on the desk instead of unhealthy snacks is a habit aready, thank you for my energy and time saved NP"
+      />
+
+      <ReviewCard
+        name="Elišká Novotná"
+        date="20.04.2021"
+        text="Zase bych Vám chtěla poděkovat za až natolik rozmanitý jídelníček a to samé za manžela"
+      />
+
+      <ReviewCard
+        name="Petr Max"
+        date="20.04.2021"
+        text="Nepíšu normálně recenze ale fakt musím říct, že jsem nedostal ještě jídlo, které by mi nechutnalo, trefíte se vždy přesně"
+      />
+
+      <ReviewCard
+        name="Šája Charlotta"
+        date="20.04.2021"
+        text="Nadšena jsem zákaznickým přístupem, úplně jiný, řekla bych západní level"
+      />
+
+      <ReviewCard
+        name="Adam Kr"
+        date="20.04.2021"
+        text="možná jedna z nejlepších podobných firem, zkoušel jsem jich mnoho a po prvním měsici s vámi už vím, že zůstávám"
+      />
+
+      <ReviewCard
+        name="Kristynka"
+        date="20.04.2021"
+        text="dnešní večeře naprosto bomba nelitovali jsme že jsme nešli pro jídlo ven"
+      />
+
+      <ReviewCard
+        name="Tereza Petrova"
+        date="20.04.2021"
+        text="Bojím se vás vynachválit aby všechno zůstalo jak to je ale musím, chutnálo mi zatím absolutně každé jídlo, zejména dezerty jsou aaach"
+      />
     </div>
   )
 }
-
-
-
-
-
